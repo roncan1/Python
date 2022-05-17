1ad314ed85b3805274cabf11bfaceeb204b176fc
Jimin-Choi <65841016+roncan1@users.noreply.github.com>
2022-05-17 12:06:16 +0900

    도라에몽 완성

--- doraemon.py
+++ doraemon.py
@@ -1,7 +1,7 @@
 import turtle as t
 t.title("도라에몽")
 t.pensize(5)
-t.speed(10)
+t.speed(0)
 t.penup()
 t.goto(-100, -200)
 t.pendown()
@@ -84,4 +84,146 @@ for i in range(30):
     
 t.rt(100)
 t.circle(30)
+t.penup()
+t.goto(-40,137)
+t.pendown()
+t.rt(200)
+for i in range(22):
+    t.fd(3)
+    t.lt(1)
+    
+for i in range(16):
+    t.lt(4)
+    t.fd(3)
+t.lt(5)
+for i in range(20):
+    t.fd(3)
+    t.lt(3)
+    
+t.lt(5)
+for i in range(30):
+    t.fd(3)
+    t.lt(2)
+t.penup()
+t.goto(-50,152)
+t.pendown()
+t.lt(180)
+t.pensize(8)
+for i in range(24):
+    t.fd(2)
+    t.lt(6)
+t.pensize(5)
+t.penup()
+t.goto(-39,167)
+t.pendown()
+t.lt(33)
+for i in range(16):
+    t.fd(4)
+    t.lt(3)
+
+for i in range(37):
+    t.fd(3)
+    t.lt(3)
+t.rt(90)
+for i in range(36):
+    t.fd(9)
+    t.rt(3)
+for i in range(17):
+    t.fd(9)
+    t.rt(1)
+t.penup()
+t.goto(-39,167)
+t.pendown()
+t.rt(135)
+for i in range(27):
+    t.fd(3)
+    t.rt(2)
+t.rt(5)
+for i in range(25):
+    t.fd(3)
+    t.rt(3)
+t.rt(5)
+for i in range(27):
+    t.fd(3)
+    t.rt(2)
+t.penup()
+t.goto(-10,165)
+t.pendown()
+t.lt(165)
+t.pensize(8)
+for i in range(24):
+    t.fd(2)
+    t.rt(6)
+t.pensize(5)
+
+t.penup()    
+t.goto(-27, 77)
+t.pendown()
+t.rt(20)
+for i in range(14):
+    t.fd(6)
+    t.lt(1)
+t.penup()    
+t.goto(200, 64)
+t.pendown()
+t.rt(89)
+t.fd(355)
+t.rt(10)
+for i in range(140):
+    t.fd(1)
+    t.rt(1)
+for i in range(30):
+    t.fd(1)
+    t.rt(2)
+t.penup()    
+t.goto(200, 64)
+t.pendown()
+t.rt(75)
+for i in range(64):
+    t.fd(4)
+    t.rt(1)
+t.rt(1)
+for i in range(30):
+    t.fd(2)
+    t.rt(1)
+t.rt(1)
+for i in range(60):
+    t.fd(4)
+    t.rt(1)
+    
+t.penup()    
+t.goto(90, 56)
+t.pendown()
+t.rt(125)
+t.fd(150)
+
+t.penup()    
+t.goto(88, 76)
+t.pendown()
+t.lt(22)
+t.fd(150)
+
+t.penup()    
+t.goto(85, 96)
+t.pendown()
+t.lt(22)
+t.fd(150)
+
+t.penup()    
+t.goto(-95, 56)
+t.pendown()
+t.lt(130)
+t.fd(130)
+
+t.penup()    
+t.goto(-95, 36)
+t.pendown()
+t.lt(25)
+t.fd(150)
+
+t.penup()    
+t.goto(-95, 16)
+t.pendown()
+t.lt(25)
+t.fd(150)
 t.mainloop()
