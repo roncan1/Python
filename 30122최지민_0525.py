c1192cdca4c8423d174d062ef28d5b2170681c25
Jimin-Choi <65841016+roncan1@users.noreply.github.com>
2022-05-25 11:02:05 +0900

    0525

---
+++ 30122최지민_0525.py
@@ -0,0 +1,40 @@
+# 1
+# import math
+# print(math.pi)
+# print(math.sqrt(4.0))
+
+# 2
+# import math as m
+# print(m.sqrt(4.0))
+
+# 3
+# from math import pi
+# print(pi)
+
+# 4
+# from math import *
+# print(pi)
+# print(sqrt(4.0))
+
+# 5
+# from math import pi as s
+# print(s(4.0))
+
+# 6
+# from math import pi as p, sqrt as s
+# print(p)
+# print(s(2.0))
+
+# 7
+# import sys
+# print(sys.builtin_module_names)
+
+# 8
+# import math
+# print(dir(math))
+
+# 9
+import ModuleExample1_0525
+ModuleExample1_0525.func1
+ModuleExample1_0525.func2
+ModuleExample1_0525.func3
